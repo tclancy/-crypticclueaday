70738647f137af790e2ae6b03da978d4afc7cc84
Tom Clancy <tclancy@gmail.com>
2025-11-19 23:18:10 -0500

    Initial commit - BlueSky feed generator for #crypticclueaday

---
+++ CLAUDE.md
@@ -0,0 +1,8 @@
+# BlueSky Feed Generator
+
+This is an attempt to build a bsky.com feed for all posts tagged with
+`#crypticclueaday` based on the instructions in README.md in this directory.
+I am hoping to publish this feed at a domain I own, `https://tomclancy.info/`
+but that is essentially a static site, hosted at GitHub pages, though it is
+powered by the Python blog engine Pelican. You can see the code in the 
+pelicantpc folder in the parent folder of this project.
---
+++ src/algos/index.ts
@@ -0,0 +1,16 @@
+import { AppContext } from '../config'
+import {
+  QueryParams,
+  OutputSchema as AlgoOutput,
+} from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
+import * as whatsAlf from './whats-alf'
+import * as crypticclueaday from './crypticclueaday'
+
+type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
+
+const algos: Record<string, AlgoHandler> = {
+  [whatsAlf.shortname]: whatsAlf.handler,
+  [crypticclueaday.shortname]: crypticclueaday.handler,
+}
+
+export default algos
